57f5eacfcfc044e84040defee22a508663dff669
HomerG <homer.g@nyu.edu>
2023-02-15 16:10:11 +0400

    added functions in header files

--- interface/cpp/src/cofhe/cofhe.cpp
+++ interface/cpp/src/cofhe/cofhe.cpp
@@ -411,9 +411,9 @@ void Cofhe::writeCommand(const uint8_t * data, uint32_t address, uint32_t size)
     }
 }
 
-void Cofhe::test_cofhe(){
-        std::cout << "Hello from Cofhee!" << std::endl;
-    }
+// void Cofhe::test_cofhe(){
+//         std::cout << "Hello from Cofhee!" << std::endl;
+//     }
 
 } // cofhe
 
