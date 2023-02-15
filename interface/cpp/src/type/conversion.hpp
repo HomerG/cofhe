6e0461c152e029c820d6b59682f1d2991bc6c1c7
HomerG <homer.g@nyu.edu>
2023-02-15 16:59:54 +0400

    adf

--- interface/cpp/src/type/conversion.hpp
+++ interface/cpp/src/type/conversion.hpp
@@ -33,15 +33,15 @@ char uint8tocharhex(uint8_t n);
 std::string uint8tostrhex(const uint8_t * n, int nBytes);
 std::vector<std::string> uint8tostrhex(const uint8_t * n, int nItems, int nBytes);
 uint64_t  uint8touint64(const uint8_t * n);
-// uint8_t * header_uint64touint8(uint64_t din)
-// {
-//     auto s    = sizeof(uint64_t);
-//     auto size = s << 1;
-//     auto dout = (uint8_t *) malloc(size);
-//     size_t i;
-//     for ( i=0; i<s   ; i++ ) dout[i] = ( din >> e3::cofhe::byte2bit(i) ) & 0xff;
-//     for (    ; i<size; i++ ) dout[i] = 0;
-//     return dout;
-// }
+uint8_t * header_uint64touint8(uint64_t din)
+{
+    auto s    = sizeof(uint64_t);
+    auto size = s << 1;
+    auto dout = (uint8_t *) malloc(size);
+    size_t i;
+    for ( i=0; i<s   ; i++ ) dout[i] = ( din >> e3::cofhe::byte2bit(i) ) & 0xff;
+    for (    ; i<size; i++ ) dout[i] = 0;
+    return dout;
+}
 
 } // conversion
